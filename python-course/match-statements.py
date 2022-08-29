02b909df73bf1113775f8a8fba6e9f22a2928dfe
Samuel Schanderl <110936852+samschanderl@users.noreply.github.com>
2022-08-29 06:15:21 +0200

    Create match-statements.py

---
+++ python-course/match-statements.py
@@ -0,0 +1,13 @@
+http_status = 200
+
+match http_status:
+	case 200 | 201:
+		print("Success")
+	case 400:
+		print("Bad request")
+	case 404:
+		print("Page not found"
+	case 500 | 501:
+		print("Server Error")
+	case _: # default class
+		print("Unknown")
